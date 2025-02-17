a80fef865025b0695535274f14fe11e55aac18a1
nitesh.pardhi <nitesh.pardhi@yamatech.in>
2025-02-17 11:48:33 +0530

    delete dashboard compo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentDetailsComponent } from './content-details/content-details.component';
 import { ContentComponent } from './content/content.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -17,7 +15,6 @@ const routes: Routes = [
   // {path: 'login', component: LoginComponent},
   { path: 'content-details', component: ContentDetailsComponent },
   { path: 'content-details/:id', component: ContentDetailsComponent },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'header', component: HeaderComponent },
 
   //Other wise Redirect To
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContentComponent } from './content/content.component';
 import { CartComponent } from './cart/cart.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDialogModule} from '@angular/material/dialog';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
 import { ContentDetailsComponent } from './content-details/content-details.component';
-import { FilterPipe } from './pipes/filter.pipe';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 
 @NgModule({
@@ -25,7 +24,6 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     ContentDetailsComponent,
     FilterPipe,
-    DashboardComponent,
     HomeComponent
   ],
   imports: [
